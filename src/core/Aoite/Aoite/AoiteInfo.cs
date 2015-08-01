c688000cee5c3d02fac73607af9b9ac79428c05d
treenew <treenew@sofire.org>
2015-08-01 11:08:36 +0800

    2.0 最后更新

--- src/core/Aoite/Aoite/AoiteInfo.cs
+++ src/core/Aoite/Aoite/AoiteInfo.cs
@@ -31,7 +31,7 @@ namespace Aoite
         /// <summary> 
         /// 程序集详细的版本。
         /// </summary>
-        public const string AssemblyVersion = Version + ".0.13";
+        public const string AssemblyVersion = Version + ".0.14";
         /// <summary>
         /// 公司。
         /// </summary>
@@ -111,7 +111,7 @@ namespace Aoite
             /// <summary> 
             /// 程序集详细的版本。
             /// </summary>
-            public const string AssemblyVersion = Version + ".0.9";
+            public const string AssemblyVersion = Version + ".0.14";
             /// <summary> 
             /// 程序集的唯一标识。
             /// </summary>
