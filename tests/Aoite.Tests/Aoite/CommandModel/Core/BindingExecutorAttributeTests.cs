932151609f7189223b46fb86428616f8fce34468
TreenewLyn <treenew@sofire.org>
2016-08-05 15:39:59 +0800

    增加缺失的单元测试。

--- tests/Aoite.Tests/Aoite/CommandModel/Core/BindingExecutorAttributeTests.cs
+++ tests/Aoite.Tests/Aoite/CommandModel/Core/BindingExecutorAttributeTests.cs
@@ -10,9 +10,15 @@ namespace Aoite.CommandModel
     public class BindingExecutorAttributeTests
     {
         [Fact]
-        public void CtorTest()
+        public void Ctor_Throw_ArgumentNullException_Test()
         {
             Assert.Equal("type", Assert.Throws<ArgumentNullException>(() => new BindingExecutorAttribute(null)).ParamName);
+        }
+
+        [Fact]
+        public void CtorTest()
+        {
+            Assert.Equal(typeof(string), new BindingExecutorAttribute(typeof(string)).Type);
         }
     }
 }
